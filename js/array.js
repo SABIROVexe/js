15fb1b429d91943de01f2395be6d35a6a8b1480b
ILNAZ-linux <sabirov.main@gmail.com>
2025-02-23 19:09:51 +0300

    сделал задание с массивом(вывод четных чисел)

--- js/array.js
+++ js/array.js
@@ -1,2 +1,17 @@
 // Задание: Напишите функцию filterEvenNumbers, которая принимает массив чисел 
 // и возвращает новый массив, содержащий только четные числа.
+
+let str = prompt("Введите массив чисел: ");
+let array = str.split("");
+
+function filterEvenNumbers() {
+    for(let i = 0; i <= array.length; i++){
+        if(array[i] % 2 == 0){
+            console.log(array[i]);
+        }
+    }
+
+}
+
+
+filterEvenNumbers();
