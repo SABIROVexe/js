df1d3646fea026222ac26b8000776cdbc446e8df
ILNAZ-linux <sabirov.main@gmail.com>
2025-02-23 06:37:14 +0300

    сделал калькулятор

--- js/calc.js
+++ js/calc.js
@@ -1,2 +1,34 @@
 /*Задание: Напишите функцию calculator, которая принимает два числа и оператор (+, -, *, /)
  и возвращает результат операции. */
+
+let term1;
+let term2;
+let operator;
+
+
+term1 = prompt("Введите первое слагаемое: ");
+term2 = prompt("Введите второе слагаемое: ");
+operator = prompt("Введите оператор: ");
+
+function calc(){
+    switch(operator){
+        case "+":
+            console.log(Number(term1) + Number(term2));
+        break;
+        
+        case "-":
+            console.log(Number(term1) - Number(term2));
+        break;
+    
+        case "/":
+            console.log(Number(term1) / Number(term2));
+        break;
+    
+        case "*":
+            console.log(Number(term1) * Number(term2));
+        break;
+    }
+}
+
+calc();
+
