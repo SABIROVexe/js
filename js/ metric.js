dfae2a850dac028f4c815aba8b8f69bef47924c6
ILNAZ-linux <sabirov.main@gmail.com>
2025-02-26 20:59:52 +0300

    сделал задание с переводом систем измерения

--- js/ metric.js
+++ js/ metric.js
@@ -1,3 +1,37 @@
 // В зависимости от выбора пользователя напечатать таблицу перевода 
 // расстояний в дюймах в сантиметры (1 дюйм = 2, 54 см) или расстояний в 
 // сантиметрах в дюймы для значений от A до B с шагом D.
+
+let a = parseFloat(prompt("Введите a: ")); 
+let b = parseFloat(prompt("Введите b: "));;
+
+let d = parseFloat(prompt("Введите шаг: "));;
+
+let choice = prompt("дюймы => сантиметры (1) / сантиметры => дюймы(2)");
+
+function processing_1 (){
+
+    for(let i = 0; i <= b; i = i + d){
+        let result1 = i * 2.54;
+        console.log("дюйм: " + i + " сантиметр: " + result1);
+    }
+}
+
+function processing_2 (){
+
+    for(let i = 0; i <= b; i = i + d){
+        let result2 = i / 2.54;
+        console.log("сантиметр: " + i + " дюйм: " + result2);
+    }
+}
+
+switch(choice){
+    case '1':
+        processing_1();
+    break;
+    
+    case '2':
+        processing_2();
+    break;
+        
+}
