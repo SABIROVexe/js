57dc9db6e2f073750c61b11f12c8f4ec0708bb54
ILNAZ-linux <sabirov.main@gmail.com>
2025-03-13 08:48:31 +0300

    изучаю объекты

--- js/object.js
+++ js/object.js
@@ -1,1 +1,23 @@
-alert ("hello world");
+let user = {
+    name: "ILNAZ",
+    age: 19,
+    "like cars": true,
+}
+
+console.log(user.name);
+console.log(user.age);
+
+user.gender = "male";
+console.log(user.gender);
+
+user.gender = "female";
+console.log(user.gender);
+
+delete user.gender;
+console.log(user.gender);
+
+console.log(user["like cars"]);
+
+delete user["like cars"];
+console.log(user["like cars"]);
+
