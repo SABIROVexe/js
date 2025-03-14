b9bfeda8b65f310c9694de909b4582214ecfaa5c
ILNAZ-linux <sabirov.main@gmail.com>
2025-03-14 08:47:59 +0300

    сделал задачу с приветствием

---
+++ js/object2.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Ilnaz",
+    age: 19,
+    city: "KAZAN",
+};
+
+function greet(){
+    alert("Привет, меня зовут " + person.name + ", мне" + person.age + "лет и я из " + person.city );
+}
+
+greet();
